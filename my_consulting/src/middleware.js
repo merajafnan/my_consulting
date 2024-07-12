205d9687384acf03ad8682ce44cc783ace5cc9c2
merajafnan <merajafnan@gmail.com>
2024-07-12 22:00:59 +0530

    my_const in progress

---
+++ my_consulting/src/middleware.js
@@ -0,0 +1,21 @@
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware({
+//     publicRoutes: ['/']
+// });
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
+    publicRoutes: ['/']
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
